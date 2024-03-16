ccd017e21f6bc9b99aa4b475c8ebfd25b0a65e91
Hashan <hemachandra.hashan@gmail.com>
2024-03-16 11:05:41 +0530

    emoji data

---
+++ constants/emojiData.tsx
@@ -0,0 +1,24 @@
+import { EmojiDataType } from "@/types/types";
+
+export const EMOJI_DATA: EmojiDataType[] = [
+  {
+    icon: "🖖",
+    name: "Prosper"
+  },
+  {
+    icon: "🤞",
+    name: "Luck"
+  },
+  {
+    icon: "👌",
+    name: 'Perfecet'
+  },
+  {
+    icon: "🤟",
+    name: "Love",
+  },
+  {
+    icon: "🙏",
+    name: "Pray",
+  },
+];
