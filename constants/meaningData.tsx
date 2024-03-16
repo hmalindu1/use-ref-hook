9cf4cd32645ec67d7b1e6544b62c02a986165972
Hashan <hemachandra.hashan@gmail.com>
2024-03-16 11:05:32 +0530

    meaning data

---
+++ constants/meaningData.tsx
@@ -0,0 +1,24 @@
+import { MeaningDataType } from "@/types/types";
+
+export const MEANING_DATA: MeaningDataType[] = [
+  {
+    name: "Prosper",
+    description: "Live Long and Prosper",
+  },
+  {
+    name: "Luck",
+    description: "Fingers crossed, I wish you Good Luck!",
+  },
+  {
+    name: "Perfecet",
+    description: "This is Perfect!",
+  },
+  {
+    name: "Love",
+    description: "I Love You 💕",
+  },
+  {
+    name: "Pray",
+    description: "God will always be with you",
+  },
+];
