297fe0507a60d6dc66ba5e033f2c4c79ad1a6c71
Hashan <hemachandra.hashan@gmail.com>
2024-03-17 07:17:00 +0530

    ref added to MeaningCard

--- components/MeaningCard.tsx
+++ components/MeaningCard.tsx
@@ -1,13 +1,25 @@
 "use client";
 
 import { MeaningDataType } from "@/types/types";
+import { forwardRef, LegacyRef } from "react";
 
 interface MeaningCardProps {
-    meaning: MeaningDataType;
+  meaning: MeaningDataType;
 }
 
-const MeaningCard = ({ meaning }:  MeaningCardProps ) => {
-  return <div className="h-20 bg-black/5 flex items-center justify-center rounded-md shadow">{meaning.description}</div>;
-};
+const MeaningCard = forwardRef(
+  ({ meaning }: MeaningCardProps, ref: LegacyRef<HTMLDivElement> | null) => {
+    return (
+      <div
+        className="h-20 bg-black/5 flex items-center justify-center rounded-md shadow"
+        ref={ref}
+      >
+        {meaning.description}
+      </div>
+    );
+  }
+);
+
+MeaningCard.displayName = "MeaningCard";
 
 export default MeaningCard;
