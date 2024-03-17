1edc77722c56f5ea24aed0a72b59fe80add2c721
Hashan <hemachandra.hashan@gmail.com>
2024-03-17 07:19:31 +0530

    onClick props passed inside the Emoji Title

--- components/EmojiTile.tsx
+++ components/EmojiTile.tsx
@@ -4,11 +4,15 @@ import { EmojiDataType } from "@/types/types";
 
 interface EmojiTileProps {
   emoji: EmojiDataType;
+  onClick: () => void;
 }
 
-const EmojiTile = ({ emoji }: EmojiTileProps) => {
+const EmojiTile = ({ emoji, onClick }: EmojiTileProps) => {
   return (
-    <div className="w-20 h-20 shadow flex items-center justify-center rounded-md bg-black/5 hover:bg-black/10 cursor-pointer">
+    <div
+      className="w-20 h-20 shadow flex items-center justify-center rounded-md bg-black/5 hover:bg-black/10 cursor-pointer"
+      onClick={onClick}
+    >
       {emoji.icon}
     </div>
   );
