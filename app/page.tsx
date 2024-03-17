0d071ccd1b66961ca023359e05f1f24d58143adb
Hashan <hemachandra.hashan@gmail.com>
2024-03-17 07:15:22 +0530

    meaningRefs props passed

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,19 @@ const Home = () => {
   const meaningRefs = {
     Prosper: useRef(null),
     Luck: useRef(null),
-    Perfecet: useRef(null),
+    Perfect: useRef(null),
     Love: useRef(null),
     Pray: useRef(null),
   };
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-10">
+      <h1 className="text-3xl font-medium">Mastering Refs in React 🧐</h1>
       <div className="flex gap-7">
-        <EmojiRow />
+        <EmojiRow meaningRefs={meaningRefs} />
       </div>
-      <div className="flex flex-col gap-7 w-[500px] max-h-[calc(100vh-870px)] overflow-y-auto shadow-inner">
-        <MeaningList />
+      <div className="flex flex-col gap-7 w-[500px] max-h-[calc(100vh-910px)] overflow-y-auto shadow-inner">
+        <MeaningList meaningRefs={meaningRefs} />
       </div>
     </div>
   );
